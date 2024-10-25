d91d1f32fcc1f9bda71b6ca0bd34b7fbd3b833e6
Nico <94575616+dopeyduck@users.noreply.github.com>
2024-10-25 02:01:45 -0500

    Updated script
    Fixed an error in the code that was causing it to not return results.

--- find_variations.py
+++ find_variations.py
@@ -1,5 +1,7 @@
-import json
+import csv
 from lxml import etree
+import os
+from tabulate import tabulate
 
 # Define the namespace
 ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
@@ -18,22 +20,45 @@ excluded_witnesses = set(excluded_witnesses_input.replace(',', ' ').split()) if 
 # Prompt for the maximum number of other witnesses
 max_other_witnesses = int(input("Please enter the maximum number of other witnesses that may be present: "))
 
+# Prompt for the CSV file name
+csv_file_name = input("Please enter the name of the CSV file to save the results: ")
+csv_file_path = os.path.join(os.path.dirname(__file__), csv_file_name)
+
 # Open and parse the XML file
 with open(xml_file_path, 'r') as file:
     tree = etree.parse(file)
 
-# Iterate over all 'app' elements
-for app in tree.xpath('//tei:app', namespaces=ns):
-    rdgs = app.xpath('.//tei:rdg', namespaces=ns)
-    for rdg in rdgs:
-        wit_set = set(rdg.get('wit', '').split())
-        if not wit_set:
-            continue
-        # Check if any excluded witnesses are present
-        if wit_set & excluded_witnesses:
-            continue
-        included_count = len(wit_set & included_witnesses)
-        other_witnesses_count = len(wit_set - included_witnesses)
-        if included_count > 0 and other_witnesses_count <= max_other_witnesses:
-            witnesses_str = ' '.join(wit_set)
-            print(f"{app.get('n')}\t{app.get('from')}\t{app.get('to')}\t{rdg.get('n')}\t{witnesses_str}\t{rdg.text}")
+# Find all variation units
+variation_units = tree.xpath('//tei:app', namespaces=ns)
+
+# Prepare the results list
+results = []
+
+# Process each variation unit
+for vu in variation_units:
+    vu_id = vu.get('n')
+    vu_from = vu.get('from')
+    vu_to = vu.get('to')
+    
+    # Process each reading within the variation unit
+    for rdg in vu.xpath('tei:rdg', namespaces=ns):
+        rdg_witnesses = set(rdg.get('wit').split())
+        rdg_varSeq = rdg.get('varSeq')
+        rdg_n = rdg.get('n')
+        rdg_type = rdg.get('type')
+        
+        # Check if the reading includes all included witnesses and excludes all excluded witnesses
+        if included_witnesses.issubset(rdg_witnesses) and excluded_witnesses.isdisjoint(rdg_witnesses):
+            # Check the number of other witnesses
+            other_witnesses = rdg_witnesses - included_witnesses
+            if len(other_witnesses) <= max_other_witnesses:
+                results.append([vu_id, vu_from, vu_to, rdg_n, rdg_varSeq, rdg_type, ' '.join(rdg_witnesses)])
+
+# Write the results to a CSV file
+with open(csv_file_path, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Variation Unit', 'From', 'To', 'Reading', 'VarSeq', 'Type', 'Witnesses'])
+    writer.writerows(results)
+
+# Print the results in a tabular format
+print(tabulate(results, headers=['Variation Unit', 'From', 'To', 'Reading', 'VarSeq', 'Type', 'Witnesses']))
